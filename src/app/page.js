2bf2e70bedd55609f06c3a2ca16b90a7f73ac272
Bellussi <enzobellussifam@gmail.com>
2025-03-02 19:22:29 -0300

    delete: remocao de componentes nao mais usados

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import DynamicMenuLoader from '@/components/menu/DynamicMenu.client';
-
-export default function Home() {
-  return (
-    <main>
-      <DynamicMenuLoader />
-    </main>
-  );
-}
--- src/services/cartService.js
+++
@@ -1,7 +0,0 @@
-export const saveCart = (cart) => {
-  localStorage.setItem('cart', JSON.stringify(cart));
-};
-
-export const loadCart = () => {
-  return JSON.parse(localStorage.getItem('cart')) || [];
-};
